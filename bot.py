9a282cb573e3201e3f39dc3ca99608b1b2a976a0
Ricart <ricarteusuo11@gmail.com>
2020-05-25 15:39:06 -0300

    feat: adiciona citações a b99

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import os
 
 import discord
 from dotenv import load_dotenv
+import random
+
+from messages import BROKLYN_99_QUOTES
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,5 +24,15 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '99!':
+        response = random.choice(BROKLYN_99_QUOTES)
+        await message.channel.send(response)
 
 client.run(TOKEN)
---
+++ messages.py
@@ -0,0 +1,9 @@
+BROKLYN_99_QUOTES = [
+    'Title of your sex tape.',
+    'The English language can not fully capture the depth and complexity of my thoughts, so I’m incorporating emojis into my speech to better express myself. Winky face.',
+    'A place where everybody knows your name is hell. You’re describing hell.',
+    'Cool, cool, cool, cool, cool. No doubt, no doubt, no doubt.',
+    'Captain Wuntch. Good to see you. But if you’re here, who’s guarding Hades?',
+    'Well, no one asked you. It’s a self-evaluation.',
+    'I don’t want to hang out with some stupid baby who’s never met Jake.'
+]
